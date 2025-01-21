4191c021469f5ce2935c5be905b1a54e86553ff9
shre-ramki <98550021+shre-ramki@users.noreply.github.com>
2025-01-21 23:34:04 +0530

    Update first.py

--- first.py
+++ first.py
@@ -20,8 +20,8 @@ if page == "Home":
     product_data['Category'] = product_data['Category'].str.lower()
 
     # Initialize NLPCloud Clients (API key should ideally be in secrets.toml)
-    summarization_client = nlpcloud.Client("finetuned-llama-3-70b", "27efa52a9f899789d8da2aa0b6ac5b2dc8c16bbb", gpu=True)
-    sentiment_client = nlpcloud.Client("distilbert-base-uncased-emotion", "27efa52a9f899789d8da2aa0b6ac5b2dc8c16bbb", gpu=False)
+    summarization_client = nlpcloud.Client("finetuned-llama-3-70b", "83a659c2d8f5e3431032fecd0e66c17025b37a8b", gpu=True)
+    sentiment_client = nlpcloud.Client("distilbert-base-uncased-emotion", "83a659c2d8f5e3431032fecd0e66c17025b37a8b", gpu=False)
 
     # Search functionality
     search_term = st.text_input("Search for a Brand or Category", "").lower()
